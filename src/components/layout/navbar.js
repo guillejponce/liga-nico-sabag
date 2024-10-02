46b79cab7512a0b31d408b101628a64f7cf5799c
GUILLERMO PONCE <guillejponce@uc.cl>
2024-10-01 23:19:44 -0300

    feat: add first navbar and style libraries

---
+++ src/components/layout/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4">
+      <div className="container mx-auto">
+        <h1 className="text-white text-2xl font-bold">Liga Nico Sabag</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../components/layout/navbar';
+import logo from '../assets/images/logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

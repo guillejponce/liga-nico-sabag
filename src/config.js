9937066d5b4d84f7d9677a2ef1fd2a1843a637a9
Federico Alarcon <afalarconm@uc.cl>
2025-10-14 12:21:21 -0300

    fix: update backend URL to point to the new domain

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 // Use an environment variable for the backend URL
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://api.liganicosabag.me';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://api.liganicosabag.cl';
 
 // Create a PocketBase instance
 const pb = new PocketBase(BACKEND_URL);
